7bdb3018db6a4b6a93820da864433d1bfcd8522a
inch6134 <148062152+inch6134@users.noreply.github.com>
2024-02-12 16:28:14 -0800

    Initialize files and define placeholders

---
+++ index.js
@@ -0,0 +1,30 @@
+// state of the game
+let state = {};
+
+// references to HTML elements
+const canvas = document.getElementById("game");
+
+newGame();
+
+function newGame() {
+    // Initialize game state
+    state = {
+
+    }
+
+    draw();
+}
+
+function draw(){
+
+}
+
+// Event handlers
+
+function throwBomb(){
+
+}
+
+function animate(timestamp){
+    
+}
